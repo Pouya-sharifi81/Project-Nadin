8abb63470ef453fa98d4807f412abb3875642a07
pouya <87375922+Pouya-sharifi81@users.noreply.github.com>
2024-02-07 22:33:19 +0330

    Add files via upload

---
+++ Nadin-Domain/Models/Pruducts/Product.cs
@@ -0,0 +1,49 @@
+﻿using Nadin_Domain.Models.user;
+using Nadin_Domain.Models.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nadin_Domain.Models.Pruducts
+{
+    public class Product
+    {
+        public Guid productId { get; set; }
+
+        public Guid UserId { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool IsAvailable { get; set; }
+        public DateTime Date { get; set; }
+        public string Name { get; set; }
+
+        public static Product CreateProduct(Guid userProfileId, string name, string email, string phone, DateTime date
+            , bool isAvailible)
+        {
+            var objectValid = new Product()
+            {
+                UserId = userProfileId,
+                Name = name,
+                Email = email,
+                Date = date,
+                Phone = phone,
+                IsAvailable = isAvailible,
+            };
+
+            return objectValid;
+
+
+        }
+        public void UpdateProduct(string name , string phone , string email , bool isavailble , DateTime date)
+        {
+            Name = name;
+           Email = email;
+           Phone = phone;
+            Date = date;
+            Phone = phone;
+            IsAvailable = isavailble;
+        }
+    }
+}
---
+++ Nadin-Domain/Validator/ProductValidator/ProductValid.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using Nadin_Domain.Models.Pruducts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nadin_Domain.Validator.ProductValidator
+{
+    public class ProductValid : AbstractValidator<Product>
+    {
+        public ProductValid()
+        {
+            RuleFor(p => p.Email)
+          .NotNull().WithMessage("email content can't be null")
+          .NotEmpty().WithMessage("Pemail content can't be empty")
+           .MinimumLength(3).WithMessage("Last name must be at least 3 characters long")
+           .MaximumLength(50).WithMessage("Last name can contain at most 50 characters long");
+            RuleFor(p => p.Name)
+         .NotNull().WithMessage("name content can't be null")
+         .NotEmpty().WithMessage("name content can't be empty")
+          .MinimumLength(3).WithMessage("Last name must be at least 3 characters long")
+          .MaximumLength(50).WithMessage("Last name can contain at most 50 characters long");
+        }
+    }
+}
